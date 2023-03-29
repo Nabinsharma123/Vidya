fac2a8a19c9ab65777b9685d2ff663c1184d9f83
Nabinsharma123 <nabinsharma16.ns@gmail.com>
2023-03-29 20:34:52 +0530

    commit

--- src/lib/Leaderboard.svelte
+++ src/lib/Leaderboard.svelte
@@ -54,7 +54,7 @@
 
             res.forEach((doc) => {
                 userList = [...userList, doc.data()];
-                dispatch("loaded");
+
                 console.log(doc.data());
             });
         },
@@ -101,4 +101,12 @@
             </TableBody>
         </Table>
     {/if}
+
+    <button
+        on:click={() => {
+            dispatch("back");
+        }}
+        class=" mt-4 text-white disabled:opacity-60 bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-2xl px-5 py-2 text-center"
+        >Back</button
+    >
 </div>
--- src/routes/Quiz/[id]/+page.svelte
+++ src/routes/Quiz/[id]/+page.svelte
@@ -310,16 +310,12 @@
             >
         </div>
     {:else if (Quizstatus = "Leaderboard")}
-        <div class="flex flex-col items-center">
-            <Leaderboard {subject} />
-            <button
-                on:click={() => {
-                    Quizstatus = "menu";
-                }}
-                class=" mt-4 text-white disabled:opacity-60 bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-2xl px-5 py-2 text-center"
-                >Back</button
-            >
-        </div>
+        <Leaderboard
+            {subject}
+            on:back={() => {
+                Quizstatus = "menu";
+            }}
+        />
     {/if}
 </div>
 
